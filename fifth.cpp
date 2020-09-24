a3623dc0f023a6c3c74fbeb249b5579ae1c9f499
papuht <papuht@utu.fi>
2020-09-24 10:05:46 +0300

    added fifth.cpp

---
+++ fifth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int x;
+	cout << "Input any number: \n";
+	cin >> x;
+	
+	while(x > 0) {
+		cout << "This will repeat for " << x << " more times. \n";
+		x--;
+		
+	}
+	
+
+
+}
